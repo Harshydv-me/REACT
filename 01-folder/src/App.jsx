18010c54f28bc98b23f96c1fef1363746bbafbef
Harsh Yadav <harshy050104@gmail.com>
2026-05-21 01:21:12 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <>
+     <div>
+        <h1>Hello World</h1>
+        <h3>This is my first React App</h3> 
+     </div>
+     <div id="harsh"> bleh</div>
+</>
+   )
+ }
+ 
+ export default App
+ 
