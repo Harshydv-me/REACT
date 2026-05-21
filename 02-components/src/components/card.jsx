5dae13d34cf5decffbaad5bd0b1a1f42fe960a09
Harsh Yadav <harshy050104@gmail.com>
2026-05-22 01:48:33 +0530

    Add 02-components folder

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div>
+    <h1>Card Component</h1>
+    <h2>This is a card component</h2>
+    <h3>i am harsh yadav</h3>
+    </div>;
+    </div>
+  )
+}
+
+export default card
+
---
+++ 02-components/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            Navbar Component 
+        </div>
+    </div>
+  )
+}
+
+export default navbar
