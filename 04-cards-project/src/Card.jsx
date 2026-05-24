cb232379c8ee0dceab95c86fe0b2f594fb52c637
Harsh Yadav <harshy050104@gmail.com>
2026-05-25 02:04:47 +0530

    Add 04-cards-project and consolidate all projects into root repository

---
+++ 04-cards-project/src/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Bookmark } from 'lucide-react'
+
+const Card = (props) => {
+    console.log(props)
+  return (
+    <div>
+      <div className='card'>
+        <div className="top">
+          <img src={props.logo} alt="Profile Image" />
+          <button>Save <Bookmark size={16}/></button>
+        </div>
+
+        <div className="center">
+           <h3>{props.company} <span>{props.date}</span></h3> 
+           <h2>{props.position}</h2>
+           <div className="tag">
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
+           </div>
+        </div>
+        
+        <div className="bottom">
+          <div>
+              <h3>{props.pay}</h3>
+              <p>{props.location}</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
